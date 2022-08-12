625abba69d1ee7f1a001975a966d1ce132b4f84a
Celsia-Angelin <106945534+Celsia-Angelin@users.noreply.github.com>
2022-08-12 23:08:10 +0530

    Delete PlacementManagementSystem directory

--- PlacementManagementSystem/src/main/java/com/tns/placementmanagementsystem/client/PlacementRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tns.placementmanagementsystem.client;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface PlacementRepository extends JpaRepository<Placement, Long> 
-{
-	
-}
